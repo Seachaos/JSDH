42483bfa489933302afb429094f6aeb19a24e8e1
Seachaos <ye.jian.yin@gmail.com>
2016-06-15 01:32:36 +0800

    has bug, backup

--- JSDH_Rails/app/controllers/home_controller.rb
+++ JSDH_Rails/app/controllers/home_controller.rb
@@ -1,5 +1,63 @@
+
+require 'prime'
+
 class HomeController < ApplicationController
 
+	protect_from_forgery :except => [:jsdh_prime]
 	def index
+		jsdh_init
+	end
+
+	def jsdh_prime
+		logger.info params.to_json.to_s
+		if params[:caa].present? and session[:jsdh_b].present? then
+			jsdh_set_key if params[:caa] != 'none'
+		end
+
+		jsdh_init unless session[:jsdh_g].present?
+		jsdh_init unless session[:jsdh_p].present?
+		session[:jsdh_b] = rand_prime
+		session[:jsdh_bb] = (session[:jsdh_g] **  session[:jsdh_b]) % session[:jsdh_p]
+		render :json => {
+			:bb => session[:jsdh_bb],
+			:p => session[:jsdh_p],
+			:g => session[:jsdh_g],
+			:table => Prime.take(10**5)
+		}
+	end
+
+	def key
+		render :json => {
+			:key => session[:jsdh_key]
+		}
+	end
+
+protected
+	
+	def jsdh_init
+		@jsdh_g = rand_number
+		@jsdh_p = rand_prime
+
+		session[:jsdh_key] = ''
+		session[:jsdh_g] = @jsdh_g
+		session[:jsdh_p] = @jsdh_p
+	end
+
+	def jsdh_set_key
+		aa = params[:caa].to_i
+		return false if aa < 10
+		key = ( aa ** session[:jsdh_b] ) % session[:jsdh_p]
+		session[:jsdh_key] = session[:jsdh_key] + key.to_s + 'x';
+	end
+
+	def rand_prime
+		prime = Prime.take(1000)
+		offset = 300
+		offset = offset + (rand * (prime.length - offset )).to_i - 2
+		prime[offset]
+	end
+
+	def rand_number
+		(rand * 1E6).to_i
 	end
 end
--- src/jsdh.js
+++ src/jsdh.js
@@ -23,3 +23,71 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
+
+
+function JSDH(){
+	this.shakeTimes = parseInt(Math.random() * 3) + 3;
+	this.maxShakeTimes = this.shakeTimes;
+	this.key = '';
+	this.finish = false;
+	this.aa = 'none';
+};
+
+JSDH.prototype.onShakeProgress = function(){
+	var progress = (this.maxShakeTimes - this.shakeTimes) / (this.maxShakeTimes+1) * 100;
+	this.progressCallback(parseInt(progress));
+}
+
+JSDH.prototype.shakeHand = function(){
+	var me = this;
+	$.ajax({
+		url: me.shakeUrl,
+		type: 'POST',
+		data: { caa : me.aa, finish: me.finish },
+		dataType: 'json',
+		success: function(data){
+			data.g = new BigNumber(data.g);
+			data.p = new BigNumber(data.p);
+			data.bb = new BigNumber(data.bb);
+
+			var offset = 70;
+			offset = (data.table.length - offset) * Math.random() + offset;
+			offset = parseInt(offset / 6);
+			var a = data.table[offset];
+			a = new BigNumber(a);
+			var aa = data.g.toPower(a).mod(data.p);
+			if(aa<10){
+				me.aa = 'none';
+				me.shakeHand();
+				return;
+			}
+			if(me.finish){
+				me.successCallback(me.key);
+				return;
+			}
+			var key = data.bb.toPower(a).mod(data.p);
+			me.key += key.toString() + 'x';
+			me.aa = aa.toString();
+
+			console.log('shake:'+aa);
+			if (me.shakeTimes -- > 0){
+				me.onShakeProgress();
+				me.shakeHand();
+			}else{
+				me.finish = true;
+				me.shakeHand();
+			}
+		}, error: function(raw){
+			me.failedCallback();
+		}
+	});
+}
+
+JSDH.prototype.init = function(arg){
+	this.successCallback = arg.onSuccess;
+	this.progressCallback = arg.onShake;
+	this.failedCallback = arg.onFailed;
+	this.shakeUrl = arg.prime_url;
+	this.shakeHand();
+}
+
